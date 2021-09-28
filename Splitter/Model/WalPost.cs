a5db9845cb112bbc324009e39b0f7be4af622445
zjedlin <57332121+zjedlin@users.noreply.github.com>
2021-09-28 21:53:08 +0200

    commit
    commit

---
+++ Splitter/Model/WalPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Splitter.Model
+{
+    public class WallPost
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Created { get; set; } = DateTime.Now;
+
+        public DateTimeOffset? LastUpdated { get; set; }
+
+        public DateTimeOffset? Deleted { get; set; }
+
+        public virtual int User { get; set; }
+
+        public virtual string Content { get; set; }
+    }
+}
